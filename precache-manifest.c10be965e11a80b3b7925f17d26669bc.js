4dc8e205ee86f4b6bfba7e456534208b6155cac4
Stas Sribnyi <stas.sribnyi@gmail.com>
2019-09-19 15:18:56 +0100

    Updates

---
+++ precache-manifest.c10be965e11a80b3b7925f17d26669bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04054310e8bf9781e3e4794844d27bdf",
+    "url": "/space-cats/index.html"
+  },
+  {
+    "revision": "46d89e8d7d3cf7cd3f77",
+    "url": "/space-cats/static/css/main.2db04941.chunk.css"
+  },
+  {
+    "revision": "3828e83819259610d8ed",
+    "url": "/space-cats/static/js/2.7bd8eb7e.chunk.js"
+  },
+  {
+    "revision": "46d89e8d7d3cf7cd3f77",
+    "url": "/space-cats/static/js/main.b7cd39f3.chunk.js"
+  },
+  {
+    "revision": "dd03e1812cf13300d35b",
+    "url": "/space-cats/static/js/runtime~main.50626e7f.js"
+  }
+]);
